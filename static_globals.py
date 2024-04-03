198e88634396ef34f75c81ebf595d6bef03888c1
James Baker <jlbaker361@gmail.com>
2024-04-03 19:15:09 -0400

    metric names

--- static_globals.py
+++ static_globals.py
@@ -3,3 +3,12 @@ ELITE="elite"
 RIVAL="rival"
 IP_ADAPTER="adapter"
 CHOSEN="chosen"
+
+NEGATIVE="over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
+
+#metrics:
+PROMPT_SIMILARITY="prompt_similarity"
+IDENTITY_CONSISTENCY="identity_consistency"
+TARGET_SIMILARITY="target_similarity"
+AESTHETIC_SCORE="aesthetic_score"
+IMAGE_REWARD="image_reward"
