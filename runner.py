d0036058c23b1d836970ba4fb702cb59fbff93f6
James Baker <jlbaker361@gmail.com>
2024-04-05 00:38:58 -0400

    who knwos

--- runner.py
+++ runner.py
@@ -22,20 +22,25 @@ import argparse
 from run_and_evaluate import evaluate_one_sample
 from datasets import load_dataset
 import wandb
+from gpu import print_details
 
 parser=argparse.ArgumentParser()
 
 parser.add_argument("--limit",type=int,default=50)
 parser.add_argument("--method_name",type=str,default=BLIP_DIFFUSION)
-parser.add_argument("--src_dataset",type=str,default="jlbaker361/league-optimal-prompt")
+parser.add_argument("--src_dataset",type=str,default="jlbaker361/league-hard-prompt")
 parser.add_argument("--num_inference_steps",type=int,default=30)
 parser.add_argument("--image_dir",type=str,default="/scratch/jlb638/prompt-images")
 parser.add_argument("--convergence_scale",type=float,default=0.75)
+parser.add_argument("--n_img_chosen",type=int,default=64)
+parser.add_argument("--target_cluster_size",type=int,default=10)
+parser.add_argument("--min_cluster_size",type=int,default=5)
 
 def main(args):
     accelerator=Accelerator(log_with="wandb")
     accelerator.init_trackers(project_name="one_shot")
     dataset=load_dataset(args.src_dataset,split="train")
+    print('dataset.column_names',dataset.column_names)
     aggregate_dict={
         metric:[] for metric in METRIC_LIST
     }
@@ -53,7 +58,12 @@ def main(args):
                                                               src_image,
                                                               text_prompt,
                                                               evaluation_prompt_list,
-                                                              accelerator,subject,args.num_inference_steps)
+                                                              accelerator,subject,
+                                                              args.num_inference_steps,
+                                                              args.n_img_chosen,
+                                                              args.target_cluster_size,
+                                                              args.min_cluster_size,
+                                                              args.convergence_scale)
         os.makedirs(f"{args.image_dir}/{label}/",exist_ok=True)
         for i,image in evaluation_image_list:
             path=f"{args.image_dir}/{label}/{args.method_name}_{i}.png"
@@ -72,6 +82,7 @@ def main(args):
             "result_table":wandb.Table(columns=columns,data=data)
         })
 if __name__=='__main__':
+    print_details()
     args=parser.parse_args()
     print(args)
     main(args)
