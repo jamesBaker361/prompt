1e2a16098455459e590a30bd39480a88c031c489
James Baker <jlbaker361@gmail.com>
2024-04-05 00:38:47 -0400

    if ->elif

--- run_and_evaluate.py
+++ run_and_evaluate.py
@@ -9,27 +9,163 @@ os.environ["HF_HOME"]=cache_dir
 os.environ["HF_HUB_CACHE"]=cache_dir
 import torch
 torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
+from diffusers.pipelines import BlipDiffusionPipeline
+from diffusers import AutoencoderKL, DDPMScheduler, DiffusionPipeline, StableDiffusionPipeline
 from PIL import Image
 import ImageReward as image_reward
 reward_cache="/scratch/jlb638/ImageReward"
 from static_globals import *
+from accelerate import Accelerator
+from transformers import CLIPProcessor, CLIPModel,ViTImageProcessor, ViTModel
+import numpy as np
+from peft import LoraConfig, get_peft_model
+from aesthetic_reward import get_aesthetic_scorer
+from chosen_helpers import get_hidden_states,get_best_cluster_kmeans,get_init_dist,loop
 
 def evaluate_one_sample(
         method_name:str,
         src_image: Image,
         text_prompt:str,
-        evaluation_prompt_list:list
+        evaluation_prompt_list:list,
+        accelerator:Accelerator,
+        subject:str,
+        num_inference_steps:int,
+        n_img_chosen:int,
+        target_cluster_size:int,
+        min_cluster_size:int,
+        convergence_scale:float
 )->dict:
+    method_name=method_name.strip()
     if method_name == BLIP_DIFFUSION:
-        pass
+        blip_diffusion_pipe=BlipDiffusionPipeline.from_pretrained(
+            "Salesforce/blipdiffusion", torch_dtype=torch.float32)
+        blip_diffusion_pipe=accelerator.prepare(blip_diffusion_pipe)
+        evaluation_image_list=[
+            blip_diffusion_pipe(
+                evaluation_prompt.format(subject),
+                src_image,
+                subject,
+                subject,
+                guidance_scale=7.5,
+                num_inference_steps=num_inference_steps,
+                neg_prompt=NEGATIVE,
+                height=512,
+                width=512,
+                ).images[0] for evaluation_prompt in evaluation_prompt_list
+        ]
     elif method_name==ELITE:
         pass
     elif method_name==RIVAL:
         pass
-    elif method_name==IP_ADAPTER:
-        pass
+    elif method_name==IP_ADAPTER or method_name==FACE_IP_ADAPTER:
+        pipeline=StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",safety_checker=None)
+        unet=pipeline.unet
+        vae=pipeline.vae
+        tokenizer=pipeline.tokenizer
+        text_encoder=pipeline.text_encoder
+        pipeline("none",num_inference_steps=1) #things initialize weird if we dont do it once
+        if method_name==IP_ADAPTER:
+            pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter-plus_sd15.bin")
+        elif method_name==FACE_IP_ADAPTER:
+            pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter-plus-face_sd15.bin")
+        image_encoder=pipeline.image_encoder
+        unet,text_encoder,vae,tokenizer,image_encoder = accelerator.prepare(
+            unet,text_encoder,vae,tokenizer,image_encoder
+        )
+        for model in [vae,unet,text_encoder, image_encoder]:
+            model.requires_grad_(False)
+        evaluation_image_list=[
+            pipeline(evaluation_prompt.format(subject),
+                    num_inference_steps=num_inference_steps,
+                    negative_prompt=NEGATIVE,
+                    safety_checker=None,
+                    ip_adapter_image=src_image).images[0] for evaluation_prompt in evaluation_prompt_list
+        ]
     elif method_name==CHOSEN:
-        pass
+        vit_processor = ViTImageProcessor.from_pretrained('facebook/dino-vitb16')
+        vit_model = ViTModel.from_pretrained('facebook/dino-vitb16')
+        pipeline=StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",safety_checker=None)
+        unet=pipeline.unet
+        vae=pipeline.vae
+        tokenizer=pipeline.tokenizer
+        text_encoder=pipeline.text_encoder
+        for model in [vae,unet,text_encoder, image_encoder]:
+            model.requires_grad_(False)
+        config = LoraConfig(
+            r=4,
+            lora_alpha=16,
+            target_modules=["to_k", "to_q", "to_v", "to_out.0"],
+            lora_dropout=0.0,
+            bias="none")
+        unet = get_peft_model(unet, config)
+        unet.train()
+        unet.print_trainable_parameters()
+        for model in [vae,unet,text_encoder]:
+            trainable_parameters+=[p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.AdamW(
+            trainable_parameters,
+            lr=0.0001,
+            betas=(0.9, 0.999),
+            weight_decay=0.01,
+            eps=0.00000001)
+        unet,text_encoder,vae,tokenizer = accelerator.prepare(
+            unet,text_encoder,vae,tokenizer
+        )
+        n_clusters=n_img_chosen // target_cluster_size
+        image_list=[
+                pipeline(text_prompt,negative_prompt=NEGATIVE,num_inference_steps=num_inference_steps,safety_checker=None).images[0] for _ in range(n_img_chosen)]
+        print("generated initial sets of images")
+        last_hidden_states=get_hidden_states(image_list,vit_processor,vit_model)
+        print("last hidden staes")
+        init_dist=get_init_dist(last_hidden_states)
+        print("init_dist")
+        pairwise_distances=init_dist
+        iteration=0
+        while pairwise_distances>=convergence_scale*init_dist and iteration<10:
+            iteration+=1
+            valid_image_list, pairwise_distances=get_best_cluster_kmeans(image_list, n_clusters, min_cluster_size, vit_processor, vit_model)
+            text_prompt_list=[text_prompt]*len(valid_image_list)
+            pipeline=loop(
+                valid_image_list,
+                text_prompt_list,
+                pipeline,
+                0,
+                optimizer,
+                accelerator,
+                1,
+                num_inference_steps,
+                size=512,
+                train_batch_size=2,
+                noise_offset=0.0,
+                max_grad_norm=1.0
+            )
+            image_list=[pipeline(text_prompt,num_inference_steps=num_inference_steps,safety_checker=None).images[0] for _ in range(n_img_chosen) ]
+        evaluation_image_list=[
+            pipeline(evaluation_prompt.format(text_prompt),
+                    num_inference_steps=num_inference_steps,
+                    negative_prompt=NEGATIVE,
+                    safety_checker=None).images[0] for evaluation_prompt in evaluation_prompt_list
+        ]
     else:
-        raise Exception(f"no support for {method_name}")
-    return {}
+        message=f"no support for {method_name} try one of "+" ".join(METHOD_LIST)
+        raise Exception(message)
+
+    metric_dict={}
+    clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    clip_inputs=clip_processor(text=evaluation_prompt_list, images=evaluation_image_list, return_tensors="pt", padding=True)
+
+    outputs = clip_model(**clip_inputs)
+    text_embed_list=outputs.text_embeds.detach().numpy()
+    image_embed_list=outputs.image_embeds.detach().numpy()
+    ir_model=image_reward.load("ImageReward-v1.0",download_root=reward_cache)
+
+    #for evaluation_image,evaluation_prompt in zip(evaluation_image_list, evaluation_prompt_list):
+    metric_dict[IMAGE_REWARD]=np.mean(
+        [ir_model.score(evaluation_prompt.format(subject),evaluation_image) for evaluation_prompt,evaluation_image in zip(evaluation_prompt_list, evaluation_image_list) ]
+    )
+    aesthetic_scorer=get_aesthetic_scorer()
+    metric_dict[AESTHETIC_SCORE]=np.mean(
+        [aesthetic_scorer(evaluation_image).cpu().numpy()[0] for evaluation_image in evaluation_image_list]
+    )
+    return metric_dict,evaluation_image_list
