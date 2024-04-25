bb089e1ebdc5d5354d11a91f457e68d38dc1af42
James Baker <jlbaker361@gmail.com>
2024-04-25 15:52:13 -0400

    ddpo stuff

--- static_globals.py
+++ static_globals.py
@@ -5,8 +5,14 @@ IP_ADAPTER="adapter"
 FACE_IP_ADAPTER="face_adapter"
 CHOSEN="chosen"
 INSTANT="instant"
+CHOSEN_K="chosen_k"
+CHOSEN_STYLE="chosen_style"
+CHOSEN_K_STYLE="chosen_k_style"
+DDPO="ddpo"
 
-METHOD_LIST=[BLIP_DIFFUSION, ELITE, RIVAL,IP_ADAPTER,FACE_IP_ADAPTER,CHOSEN,INSTANT]
+PLACEHOLDER="<S>"
+
+METHOD_LIST=[BLIP_DIFFUSION, ELITE, RIVAL,IP_ADAPTER,FACE_IP_ADAPTER,CHOSEN,INSTANT,DDPO]
 
 NEGATIVE="over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
 
